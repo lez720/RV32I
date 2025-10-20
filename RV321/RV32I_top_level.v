cd2754d9b384e8676e1f9a520a484029802ad2a0
ryan <eryanbenuelo@gmail.com>
2025-10-21 00:41:42 +0800

    update top level module

--- RV321/RV32I_top_level.v
+++ RV321/RV32I_top_level.v
@@ -1,1 +1,11 @@
-module RV32I_top_level #()();
+module RV32I_top_level #(
+    parameter
+        WIDTH = 32
+) (
+    input   [WIDTH-1:0] data_in,
+    output  [WIDTH-1:0] data_out
+);
+
+    wire [WIDTH-1:0]    inst_data;
+    
+endmodule
